6efd3e2a25cda8eafdf46f4e472741067247fbca
Willie Wheeler <willie.wheeler@gmail.com>
2013-05-01 22:25:39 -0700

    Cleanup

--- src/main/java/com/springinpractice/ch05/mvc/HelloWorldController.java
+++ src/main/java/com/springinpractice/ch05/mvc/HelloWorldController.java
@@ -1,9 +1,18 @@
-// Source project: sip05, branch: 01 (Maven Project)
+/* 
+ * Copyright (c) 2013 Manning Publications Co.
+ * 
+ * Book: http://manning.com/wheeler/
+ * Blog: http://springinpractice.com/
+ * Code: https://github.com/springinpractice
+ */
 package com.springinpractice.ch05.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author Joshua White
+ */
 @Controller
 public class HelloWorldController {
 
